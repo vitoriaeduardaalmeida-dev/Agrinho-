b946aad76db2dfceb0df94375156f55f110d4e1f
Vitória Eduarda de Almeida <vitoria.eduarda.almeida@escola.pr.gov.br>
2026-04-27 09:05:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+// contador de ações sustentáveis
+let total = 0;
 
+function adicionarAcao() {
+  total++;
+  document.getElementById("contador").innerText = total;
+}
+
+// mostrar práticas
+function mostrarPraticas() {
+  const lista = document.getElementById("lista");
+
+  if (lista.classList.contains("hidden")) {
+    lista.classList.remove("hidden");
+  } else {
+    lista.classList.add("hidden");
+  }
+}
+
+// mensagem personalizada
+function mensagem() {
+  let nome = document.getElementById("nome").value;
+
+  if (nome === "") {
+    document.getElementById("resposta").innerText = "Digite seu nome!";
+  } else {
+    document.getElementById("resposta").innerText =
+      "Obrigado, " + nome + ", por apoiar o agro sustentável!";
+  }
+}
+
+// modo escuro
+let dark = false;
+
+document.getElementById("temaBtn").addEventListener("click", () => {
+  dark = !dark;
+  document.body.classList.toggle("dark");
+});
